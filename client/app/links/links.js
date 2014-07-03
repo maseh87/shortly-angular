8d4f53a6e15cc79867d5ca854ee155b581d0bc37
Mason <maseh87@gmail.com>
2014-07-03 00:48:52 -0700

    created template file for directive

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,14 +3,20 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   $scope.data = {};
-  var links = [];
   $scope.getLinks = function(){
     Links.getLinks().then(function(data) {
-      data.forEach(function(dataObj) {
-        links.push(dataObj);
-      })
-      $scope.data.links = links;
+      $scope.data.links = data;
     });
   };
   $scope.getLinks();
+})
+.directive('card', function() {
+  return {
+    restrict: 'E',
+    link: function(scope, element, attr) {
+
+    },
+    templateUrl: 'app/links/link.tpl.html',
+    replace: true
+  };
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -80,11 +80,4 @@ angular.module('shortly.services', [])
     isAuth: isAuth,
     signout: signout
   };
-})
-.directive('card', function() {
-  return {
-    restrict: 'A',
-    templateUrl: '',
-    scope: 'LinkController'
-  };
 });
